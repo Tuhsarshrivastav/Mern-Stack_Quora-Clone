f6fa3c555a25a567fa82700e81e930f03d4e0d6b
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 <ts3657@gmail.com>
2021-12-29 22:59:45 +0530

    added sidebar

--- client/src/components/Quora.js
+++ client/src/components/Quora.js
@@ -1,10 +1,20 @@
 import React from "react";
 import "./css/Quora.css";
+import Feed from "./Feed";
 import QuoraHeader from "./QuoraHeader";
+import Sidebar from "./Sidebar";
+import Widget from "./Widget";
 const Quora = () => {
   return (
     <div className="quora">
       <QuoraHeader />
+      <div className="quora__contents">
+        <div className="quora__content">
+          <Sidebar />
+          <Feed />
+          <Widget />
+        </div>
+      </div>
     </div>
   );
 };
