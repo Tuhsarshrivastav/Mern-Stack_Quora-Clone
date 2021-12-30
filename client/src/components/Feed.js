811144e4cc8748672b1ddc0117643538b649b24b
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 <ts3657@gmail.com>
2021-12-30 10:33:07 +0530

    added post component inside feed

--- client/src/components/Feed.js
+++ client/src/components/Feed.js
@@ -1,10 +1,12 @@
 import React from "react";
 import QuoraBox from "./QuoraBox";
-import './css/Feed.css'
+import "./css/Feed.css";
+import Post from "./Post";
 const Feed = () => {
   return (
     <div className="feed">
       <QuoraBox />
+      <Post />
     </div>
   );
 };
---
+++ client/src/components/Post.js
@@ -0,0 +1,99 @@
+import { Avatar } from "@material-ui/core";
+import {
+  ArrowDownwardOutlined,
+  ArrowUpwardOutlined,
+  ChatBubbleOutlined,
+  MoreHorizOutlined,
+  RepeatOneOutlined,
+  ShareOutlined,
+} from "@material-ui/icons";
+import React, { useState } from "react";
+import "./css/Post.css";
+import { Modal } from "react-responsive-modal";
+import "react-responsive-modal/styles.css";
+import CloseIcon from "@material-ui/icons/Close";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
+
+const Post = () => {
+  return (
+    <div className="post">
+      <div className="post__info">
+        <Avatar />
+        <h4>User Name</h4>
+        <small>Timestamp</small>
+      </div>
+      <div className="post__body">
+        <p>this is tast question</p>
+        <button className="post__btnAnswer">Answer </button>
+      </div>
+      <div className="post__footer">
+        <div className="post__footerAction">
+          <ArrowUpwardOutlined />
+          <ArrowDownwardOutlined />
+        </div>
+        <RepeatOneOutlined />
+        <ChatBubbleOutlined />
+        <div className="post__foterLeft">
+          <ShareOutlined />
+          <MoreHorizOutlined />
+        </div>
+      </div>
+      <p
+        style={{
+          color: "rgba(0,0,0,0.5)",
+          fontSize: "12px",
+          fontWeight: "bold",
+          margin: "10px 0",
+        }}
+      >
+        1 Answer
+      </p>
+      <div
+        style={{
+          margin: "5px 0px 0px 0px ",
+          padding: "5px 0px 0px 20px",
+          borderTop: "1px solid lightgray",
+        }}
+        className="post__answer"
+      >
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            width: "100%",
+            padding: "10px 5px",
+            borderTop: "1px solid lightgray",
+          }}
+          className="post-answer-container"
+        >
+          <div
+            style={{
+              display: "flex",
+              alignItems: "center",
+              marginBottom: "10px",
+              fontSize: "12px",
+              fontWeight: 600,
+              color: "#888",
+            }}
+            className="post-answered"
+          >
+            <Avatar />
+            <div
+              style={{
+                margin: "0px 10px",
+              }}
+              className="post-info"
+            >
+              <p>Username</p>
+              <span>Timestamp</span>
+            </div>
+          </div>
+          <div className="post-answer">This is test answer</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Post;
