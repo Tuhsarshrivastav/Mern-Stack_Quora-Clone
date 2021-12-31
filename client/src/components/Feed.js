bc86125b684f48fec9d80a18f4a7e4a4ef0b3f70
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 <ts3657@gmail.com>
2021-12-31 20:01:09 +0530

    added changes

--- client/src/components/Feed.js
+++ client/src/components/Feed.js
@@ -2,7 +2,6 @@ import React from "react";
 import QuoraBox from "./QuoraBox";
 import "./css/Feed.css";
 import Post from "./Post";
-import Widget from "./Widget";
 const Feed = () => {
   return (
     <div className="feed">
